dc3df69be208962eab52e838bd5fb6a30c02535b
Salavei <kano.mori@mail.ru>
2023-01-11 16:16:36 +0300

    Day 5 - 1

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -18,8 +18,7 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     # SerializerMethodField - будет искать функцию начинающуюся на get
 
     def get_price(self, instance):
-        return (instance.service.full_price -
-                instance.service.full_price * (instance.plan.discount_percent / 100))
+        return instance.price
 
     class Meta:
         model = Subscription
